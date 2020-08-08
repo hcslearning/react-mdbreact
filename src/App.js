a9763a779424dea2c4b26eccc4693a052e15ec81
HCS Learning <learning@hardcybersoft.cl>
2020-08-07 23:18:26 -0400

    ejemplo carousel con mdbreact

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Carousel from './Carousel'
 import './App.css';
 
 // MDBReact específico 
@@ -10,20 +11,27 @@ import 'mdbreact/dist/css/mdb.css';
 
 function App() {
   return (
-    <div className="App">
-      <MDBCol>
-      <MDBCard style={{ width: "22rem" }}>
-        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            Some quick example text to build on the card title and make
-            up the bulk of the card&apos;s content.
-          </MDBCardText>
-          <MDBBtn href="#">MDBBtn</MDBBtn>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
+    <div className="App container">
+      <div id="card">
+        <MDBCol>
+        <MDBCard style={{ width: "22rem" }}>
+          <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              Some quick example text to build on the card title and make
+              up the bulk of the card&apos;s content.
+            </MDBCardText>
+            <MDBBtn href="#">MDBBtn</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      </div>
+
+      <div id="carousel">
+        <Carousel />
+      </div>
+      
     </div>
   );
 }
